01c12a50d1e984d8e4df9e1bb58dca0dfbe2f9b1
Bryon Baker <bryonbakeraus@gmail.com>
2019-06-17 12:05:38 +1000

    Updated client to call openshift service. Added initial deployment specs for OpenShift

--- client/wwwroot/js/form.js
+++ client/wwwroot/js/form.js
@@ -29,7 +29,7 @@ $(function () {
         let form = $(this);
         $.ajax({
             type: "post",
-            url: "http://localhost:10000/form",
+            url: "http://abn-validator-default.apps.cluster-anz-f723.anz-f723.openshiftworkshop.com/form",
             contentType: "application/json",
             data: JSON.stringify(form.serializeFormJSON()),
             success: function (data) {
